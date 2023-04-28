b78f8feeb032850b6595ae16c76a81959b5ec225
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-04-28 15:02:22 +0700

    Add secondary and danger button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,10 @@ const Navbar = () => {
     <nav className="flex justify-between">
       <h1>Practice 1 - Listenening Test</h1>
       <div>
-        <button>Instruction</button>
-        <button>Exit</button>
+        <button className="btn-secondary px-4">Instruction</button>
+        <button className="btn-danger px-3 py-1">
+          <i className="fa-regular fa-circle-xmark"></i> Exit
+        </button>
       </div>
     </nav>
   );
