d8d89646b039b5a47adaf5e356f2414f7ac643d6
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-04-28 15:10:47 +0700

    Add navbar padding and margin

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-between">
-      <h1>Practice 1 - Listenening Test</h1>
-      <div>
-        <button className="btn-secondary px-4">Instruction</button>
-        <button className="btn-danger px-3 py-1">
-          <i className="fa-regular fa-circle-xmark"></i> Exit
-        </button>
-      </div>
-    </nav>
+    <main className="fixed bg-gray-100 w-screen left-0">
+      <nav className="flex justify-between p-4 container mx-auto">
+        <h1>Practice 1 - Listenening Test</h1>
+        <div className="flex gap-4">
+          <button className="btn-secondary px-4">Instruction</button>
+          <button className="btn-danger px-3 py-1">
+            <i className="fa-regular fa-circle-xmark"></i> Exit
+          </button>
+        </div>
+      </nav>
+    </main>
   );
 };
 
