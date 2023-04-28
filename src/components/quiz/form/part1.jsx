f68b6cb353121c42a666718e2402ffaee8bac3a4
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-04-28 19:00:19 +0700

    Add part 1 header

--- src/components/quiz/form/part1.jsx
+++ src/components/quiz/form/part1.jsx
@@ -1,5 +1,12 @@
 const Part1 = () => {
-  return <>This is part 1</>;
+  return (
+    <>
+      <header className="flex gap-2 px-4 py-2 items-center my-4 bg-sky-200">
+        <h2>Part 1</h2>
+        <p>(Question 1 - 10)</p>
+      </header>
+    </>
+  );
 };
 
 export default Part1;
